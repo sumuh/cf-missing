e1281aa2e08973feeebf07762982bed01ca3e3f6
sumuh <muhli.susanna@gmail.com>
2024-07-26 17:49:20 +0300

    Add basic metrics for evaluating counterfactuals

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,1 +1,89 @@
-# TODO
+import pandas as pd
+import numpy as np
+
+
+class Evaluator:
+
+    def __init__(self, train_data: np.array):
+        # Calculate mean absolute deviation (MAD) for each feature in train data
+        self.mads = np.apply_along_axis(self._calculate_mad, 1, train_data)
+        print(self.mads)
+
+    def _calculate_l1_norm(self, vector_1: np.array, vector_2: np.array) -> float:
+        """Calculates l1 norm (Manhattan distance).
+
+        :param np.array vector_1: vector 1
+        :param np.array vector_2: vector 2
+        :return float: l1 norm
+        """
+        return np.sum(abs(vector_1 - vector_2))
+
+    def _calculate_l2_norm(self, vector_1: np.array, vector_2: np.array) -> float:
+        """Calculates l2 norm (Euclidean distance).
+
+        :param np.array vector_1: vector 1
+        :param np.array vector_2: vector 2
+        :return float: l2 norm
+        """
+        return np.sqrt(np.sum((vector_1 - vector_2) ** 2))
+
+    def _calculate_mad(self, values: np.array) -> float:
+        """Calculates the mean absolute deviation (MAD) of values.
+
+        :param np.array values: vector of values
+        :return float: mean absolute deviation
+        """
+        if len(values) == 0:
+            return None
+        return np.sum(abs(values - np.mean(values))) / len(values)
+
+    def _get_mad_weighted_distance(
+        self, vector_1: np.array, vector_2: np.array
+    ) -> float:
+        """Calculates distance between two vectors weighted by each feature's
+        mean absolute deviation. (Weighted l1 norm)
+
+        :param np.array vector_1: vector 1
+        :param np.array vector_2: vector 2
+        :return float: _description_
+        """
+        return np.sum(abs(vector_1 - vector_2) / self.mads)
+
+    def get_average_distance_from_original(
+        self, original_vector: np.array, cf_vectors: np.array
+    ) -> float:
+        """Calculate average distance of counterfactual vectors in array
+        from original vector.
+
+        :param np.array original_vector: original input vector
+        :param np.array cf_vectors: counterfactual vectors
+        :return float: average distance of vectors from original
+        """
+        return np.mean(
+            np.apply_along_axis(
+                self._get_mad_weighted_distance, 1, cf_vectors, vector_2=original_vector
+            )
+        )
+
+    def get_diversity(self, vectors: np.array) -> float:
+        """Diversity metric from Mothilal et al. (2020). Quantifies diversity
+        within set of vectors.
+
+        :param np.array vectors: counterfactual vectors
+        :return float: diversity measure
+        """
+        sum_distances = 0
+        for i in range(len(vectors)):
+            for j in range(i + 1, len(vectors)):
+                sum_distances += self._get_mad_weighted_distance(
+                    vectors[i, :], vectors[j, :]
+                )
+        return sum_distances / (len(vectors) ** 2)
+
+    def get_count_diversity(self, vectors: np.array) -> float:
+        """Count diversity metric from Mothilal et al. (2020).
+
+        :param np.array vectors: counterfactual vectors
+        :return float: count diversity
+        """
+        pass
---
+++ test/test_evaluator.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+
+from src.evaluation import Evaluator
+
+def test_when_mad_is_integer_then_correct_value_calculated():
+    evaluator = Evaluator(np.array([[]]))
+    arr = np.array([1, 1, 3, 3])
+    mad = evaluator._calculate_mad(arr)
+    assert mad == 1
+
+def test_when_mad_is_fraction_then_correct_value_calculated():
+    evaluator = Evaluator(np.array([[]]))
+    arr = np.array([1, 1, 2, 2])
+    mad = evaluator._calculate_mad(arr)
+    assert mad == 0.5
+
+def test_when_mad_is_zero_then_correct_value_calculated():
+    evaluator = Evaluator(np.array([[]]))
+    arr = np.array([1, 1, 1, 1])
+    mad = evaluator._calculate_mad(arr)
+    assert mad == 0
+
+def test_when_input_to_mad_is_empty_then_None_returned():
+    evaluator = Evaluator(np.array([[]]))
+    arr = np.array([])
+    mad = evaluator._calculate_mad(arr)
+    assert mad == None
+
+def test_when_vectors_are_equal_then_l1_norm_is_zero():
+    evaluator = Evaluator(np.array([[]]))
+    vector_1 = np.array([1, 1, 1, 1])
+    vector_2 = np.array([1, 1, 1, 1])
+    l1_norm = evaluator._calculate_l1_norm(vector_1, vector_2)
+    assert l1_norm == 0
+
+def test_when_vectors_l1_norm_is_integer_then_correct_norm_is_returned():
+    evaluator = Evaluator(np.array([[]]))
+    vector_1 = np.array([1, 1, 1, 1])
+    vector_2 = np.array([1, 1, 1, 2])
+    l1_norm = evaluator._calculate_l1_norm(vector_1, vector_2)
+    assert l1_norm == 1
+
+def test_when_vectors_l1_norm_is_fraction_then_correct_norm_is_returned():
+    evaluator = Evaluator(np.array([[]]))
+    vector_1 = np.array([1, 1, 1, 1])
+    vector_2 = np.array([1, 1, 1, 1.5])
+    l1_norm = evaluator._calculate_l1_norm(vector_1, vector_2)
+    assert l1_norm == 0.5
+
+def test_when_vectors_are_equal_then_l2_norm_is_zero():
+    evaluator = Evaluator(np.array([[]]))
+    vector_1 = np.array([1, 1, 1, 1])
+    vector_2 = np.array([1, 1, 1, 1])
+    l2_norm = evaluator._calculate_l2_norm(vector_1, vector_2)
+    assert l2_norm == 0
+
+def test_when_vectors_l2_norm_is_integer_then_correct_norm_is_returned():
+    evaluator = Evaluator(np.array([[]]))
+    vector_1 = np.array([1, 1, 1, 1])
+    vector_2 = np.array([1.5, 1.5, 1.5, 1.5])
+    l2_norm = evaluator._calculate_l2_norm(vector_1, vector_2)
+    assert l2_norm == 1
+
+def test_when_vectors_l2_norm_is_fraction_then_correct_norm_is_returned():
+    evaluator = Evaluator(np.array([[]]))
+    vector_1 = np.array([1, 1, 1, 1])
+    vector_2 = np.array([1.25, 1.25, 1.25, 1.25])
+    l2_norm = evaluator._calculate_l2_norm(vector_1, vector_2)
+    assert l2_norm == 0.5
+
+def test_when_vectors_have_distance_zero_then_mad_weighted_distance_returns_zero():
+    evaluator = Evaluator(np.array([[]]))
+    evaluator.mads = np.array([1, 1, 1, 1])
+    vector_1 = np.array([1, 1, 1, 1])
+    vector_2 = np.array([1, 1, 1, 1])
+    dist = evaluator._get_mad_weighted_distance(vector_1, vector_2)
+    assert dist == 0
+
+def test_when_vectors_have_integer_distance_then_mad_weighted_distance_returns_correct_number():
+    evaluator = Evaluator(np.array([[]]))
+    evaluator.mads = np.array([1, 1, 1, 1])
+    vector_1 = np.array([2, 2, 2, 2])
+    vector_2 = np.array([1, 1, 1, 1])
+    dist = evaluator._get_mad_weighted_distance(vector_1, vector_2)
+    assert dist == 4
+
+def test_when_vectors_have_fraction_distance_then_get_distance_returns_correct_number():
+    evaluator = Evaluator(np.array([[]]))
+    evaluator.mads = np.array([1, 1, 1])
+    vector_1 = np.array([0.5, 0.5, 0.5])
+    vector_2 = np.array([1, 1, 1])
+    dist = evaluator._get_mad_weighted_distance(vector_1, vector_2)
+    assert dist == 1.5
+
+def test_when_vectors_are_equal_diversity_is_zero():
+    evaluator = Evaluator(np.array([[]]))
+    evaluator.mads = np.array([1, 1, 1])
+    vector_1 = np.array([[1, 1, 1]])
+    vector_2 = np.array([[1, 1, 1]])
+    vectors = np.concatenate((vector_1, vector_2), axis=0)
+    diversity = evaluator.get_diversity(vectors)
+    assert diversity == 0
+
+def test_when_vector_diversity_is_integer_then_correct_diversity_is_returned():
+    evaluator = Evaluator(np.array([[]]))
+    evaluator.mads = np.array([1, 1, 1, 1])
+    vector_1 = np.array([[1, 1, 1, 1]])
+    vector_2 = np.array([[2, 2, 2, 2]])
+    vectors = np.concatenate((vector_1, vector_2), axis=0)
+    diversity = evaluator.get_diversity(vectors)
+    assert diversity == 1
+
+def test_when_vector_diversity_is_fraction_then_correct_diversity_is_returned():
+    evaluator = Evaluator(np.array([[]]))
+    evaluator.mads = np.array([1, 1, 1])
+    vector_1 = np.array([[1, 2, 3]])
+    vector_2 = np.array([[2, 3, 4]])
+    vectors = np.concatenate((vector_1, vector_2), axis=0)
+    diversity = evaluator.get_diversity(vectors)
+    assert diversity == 0.75
